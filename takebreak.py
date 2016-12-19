e4f406ce9957db9672679a9103c9699d3ee4423a
Amit Vyas <23amitvyas@gmail.com>
2016-12-19 22:09:58 +0530

    Add files via upload

---
+++ takebreak.py
@@ -0,0 +1,9 @@
+import time
+import webbrowser
+n=1;
+print("This is now goig to start"+time.ctime())
+while n<=3:
+
+    time.sleep(5)
+    webbrowser.open("https://www.youtube.com/watch?v=v0RqJaA0xQU")
+    n = n + 1
